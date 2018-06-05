739c4568180729ea66bf5e9dd89202f11e480a17
Dmytro Fialko <dmytrofialko@gmail.com>
2018-06-05 17:20:24 +0300

    optimization project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ToolbarComponent } from './pages/home/areas/toolbar/toolbar.component';
 import { TopGalleryComponent } from './pages/home/areas/top-gallery/top-gallery.component';
 import { AboutMeComponent } from './pages/home/areas/about-me/about-me.component';
 import { PortfolioComponent } from './pages/home/areas/portfolio/portfolio.component';
 import { ServicesAndPricesComponent } from './pages/home/areas/services-and-prices/services-and-prices.component';
 import { UsefulArticlesComponent } from './pages/home/areas/useful-articles/useful-articles.component';
 import { ContactsComponent } from './pages/home/areas/contacts/contacts.component';
-import { FooterComponent } from './pages/home/areas/footer/footer.component';
 import { SwiperModule } from 'angular2-useful-swiper';
 import { AppRoutingModule } from './app-routing.modules';
 import { Pages404Component } from './pages/pages404/pages404.component';
@@ -37,15 +35,10 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { AlbumPortfolioComponent } from './pages/album-portfolio/album-portfolio.component';
-import { ToolbarAlbumPortfolioComponent } from './pages/album-portfolio/areas/toolbar-album-portfolio/toolbar-album-portfolio.component';
-import { GalleryAlbumPortfolioComponent } from './pages/album-portfolio/areas/gallery-album-portfolio/gallery-album-portfolio.component';
-import { FooterAlbumPortfolioComponent } from './pages/album-portfolio/areas/footer-album-portfolio/footer-album-portfolio.component';
 import { PageWeddingComponent } from './pages/page-wedding/page-wedding.component';
-import { ToolbarWeddingComponent } from './pages/page-wedding/areas/toolbar-wedding/toolbar-wedding.component';
 import { BannerWeddingComponent } from './pages/page-wedding/areas/banner-wedding/banner-wedding.component';
 import { ContentWeddingComponent } from './pages/page-wedding/areas/content-wedding/content-wedding.component';
 import { InfirmationWeddingComponent } from './pages/page-wedding/areas/infirmation-wedding/infirmation-wedding.component';
-import { FooterWeddingComponent } from './pages/page-wedding/areas/footer-wedding/footer-wedding.component';
 import { HeroToolbarComponent } from './pages/hero-toolbar/hero-toolbar.component';
 import { HeroFooterComponent } from './pages/hero-footer/hero-footer.component';
 import { PageLoveStoryComponent } from './pages/page-love-story/page-love-story.component';
@@ -63,14 +56,12 @@ import { ContentPregnancyComponent } from './pages/page-pregnancy/areas/content-
   declarations: [
     AppComponent,
     HomeComponent,
-    ToolbarComponent,
     TopGalleryComponent,
     AboutMeComponent,
     PortfolioComponent,
     ServicesAndPricesComponent,
     UsefulArticlesComponent,
     ContactsComponent,
-    FooterComponent,
     Pages404Component,
     PagePortfolioComponent,
     ToolbarPagePortfolioComponent,
@@ -83,15 +74,10 @@ import { ContentPregnancyComponent } from './pages/page-pregnancy/areas/content-
     TopGalleryBlogComponent,
     UsefulArticlesBlogComponent,
     AlbumPortfolioComponent,
-    ToolbarAlbumPortfolioComponent,
-    GalleryAlbumPortfolioComponent,
-    FooterAlbumPortfolioComponent,
     PageWeddingComponent,
-    ToolbarWeddingComponent,
     BannerWeddingComponent,
     ContentWeddingComponent,
     InfirmationWeddingComponent,
-    FooterWeddingComponent,
     HeroToolbarComponent,
     HeroFooterComponent,
     PageLoveStoryComponent,
